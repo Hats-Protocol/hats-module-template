f01aece496ad7206b45f078e8b35963eed2e79ce
Ido <gershido@gmail.com>
2024-04-02 18:53:19 +0300

    format

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -42,7 +42,7 @@ contract Deploy is Script {
      *       never differs regardless of where its being compiled
      *    2. The provided salt, `SALT`
      */
-    implementation = new Module{ salt: SALT}(_version /* insert constructor args here */);
+    implementation = new Module{ salt: SALT }(_version /* insert constructor args here */ );
 
     vm.stopBroadcast();
 
